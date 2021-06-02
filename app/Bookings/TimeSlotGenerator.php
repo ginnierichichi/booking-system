707d296c3f8c225bdf636f1d1a9afbea57d03a1d
Ginnie Richichi <ginnie.richichi@hotmail.co.uk>
2021-06-02 15:46:48 +0100

    error thrown for carbon interval working on it

--- app/Bookings/TimeSlotGenerator.php
+++ app/Bookings/TimeSlotGenerator.php
@@ -3,23 +3,45 @@
 namespace App\Bookings;
 
 use App\Models\Schedule;
+use App\Models\Service;
 use Carbon\CarbonInterval;
+use Carbon\CarbonPeriod;
 
 class TimeSlotGenerator
 {
     public const INCREMENT = 15;
-    protected $interval;
 
-    public function __construct(Schedule $schedule)
+    public $schedule;
+    public $service;
+
+    protected CarbonInterval $interval;
+
+    public function __construct(Schedule $schedule, Service $service)
     {
+        $this->schedule = $schedule;
+        $this->service = $service;
 
-        $this->interval = CarbonInterval::minute(self::INCREMENT);
+        $this->interval = CarbonInterval::minutes(self::INCREMENT)->toPeriod(
+            $schedule->date->setTimeFrom($schedule->start_time),
+            $schedule->date->setTimeFrom(
+                $schedule->end_time->subMinutes($service->duration)
+            )
+        );
+    }
 
-        CarbonInterval::minute(15)
-            ->toPeriod(
-                $schedule->date->setTimeFrom($schedule->start_time),
-                $schedule->date->setTimeFrom($schedule->end_time)
-            );
+
+
+    public function applyFilters(array $filters)
+    {
+        foreach($filters as $filter) {
+            if (!$filter instanceof Filter) {
+                continue;
+            }
+
+            $filter->apply($this, $this->interval);
+        }
+
+        return $this;
     }
 
     public function get()
--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,18 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Bookings\Filters\SlotsPassedTodayFilter;
+use App\Bookings\Filters\UnavailabilityFilter;
 use App\Bookings\TimeSlotGenerator;
 use App\Models\Schedule;
-use Carbon\CarbonInterval;
-use Illuminate\Http\Request;
+use App\Models\Service;
 
 class BookingController extends Controller
 {
     public function __invoke()
     {
-        $schedule = Schedule::find(3);
+        $schedule = Schedule::find(1);
+        $service = Service::find(1);
 
-        $slots = (new TimeSlotGenerator($schedule))->get();
+        $slots = (new TimeSlotGenerator($schedule, $service))
+            ->applyFilters([
+                    new SlotsPassedTodayFilter(),
+                    new UnavailabilityFilter($schedule->unavailabilites),
+                ])
+            ->get();
 
         return view('bookings.create', [
             'slots' => $slots,
