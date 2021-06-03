738479acebb99b17f03af717f58902d75ea67e34
Ginnie Richichi <ginnie.richichi@hotmail.co.uk>
2021-06-03 11:16:20 +0100

    applying appointment filters so can not book over another appointment

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Bookings\Filters\AppointmentFilter;
 use App\Bookings\Filters\SlotsPassedTodayFilter;
 use App\Bookings\Filters\UnavailabilityFilter;
 use App\Bookings\TimeSlotGenerator;
+use App\Models\Appointment;
 use App\Models\Schedule;
 use App\Models\Service;
 
@@ -15,10 +17,13 @@ class BookingController extends Controller
         $schedule = Schedule::find(1);
         $service = Service::find(1);
 
+        $appointments = Appointment::whereDate('date', '2021-06-03')->get();
+
         $slots = (new TimeSlotGenerator($schedule, $service))
             ->applyFilters([
                     new SlotsPassedTodayFilter(),
                     new UnavailabilityFilter($schedule->unavailabilities),
+                    new AppointmentFilter($appointments),
                 ])
             ->get();
 
