5a6a7b1cf5510b2320c9c0d02b2c4e6bb865a70d
Ginnie Richichi <ginnie.richichi@hotmail.co.uk>
2021-06-03 10:54:19 +0100

    added appointment migration and model

--- resources/views/bookings/create.blade.php
+++ resources/views/bookings/create.blade.php
@@ -1,7 +1,17 @@
-<div>This is the create page</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Book a Meeting Slot') }}
+        </h2>
+    </x-slot>
 
-<div>
-    @foreach($slots as $slot)
-        {{ $slot }}
-    @endforeach
-</div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-8">
+                @foreach($slots as $slot)
+                    {{ $slot }} <br>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</x-app-layout>
