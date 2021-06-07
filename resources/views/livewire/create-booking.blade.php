a0046b02ea40d03fd0acea19cfc726ad2f03590e
Ginnie Richichi <ginnie.richichi@hotmail.co.uk>
2021-06-07 11:07:32 +0100

    added form template

--- resources/views/livewire/create-booking.blade.php
+++ resources/views/livewire/create-booking.blade.php
@@ -7,13 +7,14 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-8">
-{{--                @foreach($slots as $slot)--}}
-{{--                    {{ $slot }} <br>--}}
-{{--                @endforeach--}}
             <form>
                 <label for="service" class="inline-block font-bold text-gray-700 mb-2"> Select Service  </label>
-                <select name="service" id="service" class="bg-gray-100 w-full h-10 border-none rounded-lg">
-                    <option value=""></option>
+                <select wire:model="state.service" name="service" id="service" class="bg-gray-100 w-full h-10 border-none rounded-lg">
+                    @foreach($services as $slot)
+                        <option value="{{$slot->id}}">
+                            {{ $slot->name }}
+                        </option>
+                    @endforeach
                 </select>
             </form>
         </div>
