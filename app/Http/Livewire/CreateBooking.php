4c38536f1b580c7159c406bf7229c0ca96ce4ad0
Ginnie Richichi <ginnie.richichi@hotmail.co.uk>
2021-06-07 12:32:02 +0100

    cant select an employee before selecting a service

--- app/Http/Livewire/CreateBooking.php
+++ app/Http/Livewire/CreateBooking.php
@@ -7,9 +7,40 @@ use Livewire\Component;
 
 class CreateBooking extends Component
 {
+    public $employees;
+
     public $state = [
-        'service' => null,
+        'service' => '',
+        'employee'=> '',
     ];
+
+    public function mount()
+    {
+        //initialise
+        $this->employees = collect();
+    }
+
+    public function updatedStateService($serviceId)
+    {
+        //load in list of employees
+        $this->state['employee'] = '';
+
+        if(!$serviceId) {
+            $this->employees = collect();
+            return;
+        }
+
+        $this->employees = $this->selectedService->employees;
+    }
+
+    public function getSelectedServiceProperty()
+    {
+        if (!$this->state['service']) {
+            return null;
+        }
+
+        return Service::find($this->state['service']);
+    }
 
     public function render()
     {
--- resources/views/livewire/create-booking.blade.php
+++ resources/views/livewire/create-booking.blade.php
@@ -8,14 +8,31 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-8">
             <form>
-                <label for="service" class="inline-block font-bold text-gray-700 mb-2"> Select Service  </label>
-                <select wire:model="state.service" name="service" id="service" class="bg-gray-100 w-full h-10 border-none rounded-lg">
-                    @foreach($services as $slot)
-                        <option value="{{$slot->id}}">
-                            {{ $slot->name }}
-                        </option>
-                    @endforeach
-                </select>
+                <div class="mb-6">
+                    <label for="service" class="inline-block font-bold text-gray-700 mb-2"> Select Service  </label>
+                    <select wire:model="state.service" name="service" id="service" class="bg-gray-100 w-full h-10 border-none rounded-lg">
+                       <option value="">Select a Service</option>
+                        @foreach($services as $slot)
+                            <option value="{{$slot->id}}">
+                                {{ $slot->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="mb-6 {{ !$employees->count() ? 'opacity-25' : '' }}">
+                    <label for="service" class="inline-block font-bold text-gray-700 mb-2"> Select Employee  </label>
+                    <select wire:model="state.employee" name="service" id="service" class="bg-gray-100 w-full h-10 border-none rounded-lg" {{ !$employees->count() ? 'disabled = "disabled"' : ''  }}>
+                        <option value="">Select an Employee</option>
+                        @foreach($employees as $employee)
+                            <option value="{{$employee->id}}">
+                                {{ $employee->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="mb-6">
+                    <label for="service" class="inline-block font-bold text-gray-700 mb-2">Select Appointment Time</label>
+                </div>
             </form>
         </div>
     </div>
